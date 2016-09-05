21e2d1b877584f3ed7f11ad1f4b827fd37ade11a
witold.zegarowski <witold.zegarowski@gmail.com>
2016-09-05 11:17:28 +0200

    v

--- _plugin/js/main.js
+++ _plugin/js/main.js
@@ -17,21 +17,60 @@ function callAjax(qname, callOpts) {
         }
     });
 }
- 
+
 $(document).ready(function () {
-  
-  console.log("hello there");
-  
-  var md = `
-  <div class="meta_m">
-	Rooms
-  </div>
-  `;
-  
-  $("body").append(md)
-  
-  
-  
+
+    console.log("hello there");
+
+    let rooms = {
+        'Default': {
+            m_uuid: guid()
+        },
+        'my_fun_room': {
+            m_uuid: guid()
+        }
+    };
+
+    let h_rooms = "";
+    for (const key in rooms) {
+        let room = rooms[key];
+        h_rooms += "<div id='" +room.m_uuid+"'>"+key+"</div>"
+    }
+
+
+    var show_rooms = $(`
+<div class="meta_m">
+    <div id="show_rooms">
+        Rooms
+    </div>
+    
+     <div id="rooms">
+`
+        + h_rooms +
+`
+    </div>
+    
+</div>
+  `);
+
+    let hide_rooms = true;
+    show_rooms.find("#rooms").hide();
+
+    show_rooms.find("#show_rooms").click(function () {
+        console.log("show rooms")
+
+        if (hide_rooms) {
+            show_rooms.find("#rooms").show();
+        } else {
+            show_rooms.find("#rooms").hide();
+        }
+        hide_rooms = !hide_rooms;
+
+    });
+
+    $("body").append(show_rooms);
+
+
 });
 
 function guid() {
