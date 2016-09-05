1a9a05b4aeb5f590711552837f955821664b6c00
witold.zegarowski <witold.zegarowski@gmail.com>
2016-09-05 17:09:00 +0200

    v

--- _plugin/js/main.js
+++ _plugin/js/main.js
@@ -18,16 +18,23 @@ function callAjax(qname, callOpts) {
     });
 }
 
+const user = {
+    uuid: guid(),
+    username: "witold"
+};
+
 $(document).ready(function () {
 
     console.log("hello there");
 
     let rooms = {
         'Default': {
-            m_uuid: guid()
+            m_uuid: guid(),
+            owner: user
         },
         'my_fun_room': {
-            m_uuid: guid()
+            m_uuid: guid(),
+            owner: user
         }
     };
 
@@ -39,42 +46,103 @@ $(document).ready(function () {
 
     const all_rooms = $(`
 <div class="meta_all_rooms my_draggable">
-    <div >
-        <div class="my_resizable">
-            <div>
-               <b>Meta:</b><a href="javascript:void(0)" id="show_rooms"> Show rooms</a>
-            </div>
-             <div id="rooms">
-                <ul>` + h_rooms + `</ul>
-            </div>
-        </div>
+   <b>Meta</b> <a href="javascript:void(0)" id="show_rooms">[show]</a>
+    <div id="rooms">
+        Public rooms:
+        <ul>` + h_rooms + `</ul>
+        My Rooms:
+        <b><a href="javascript:void(0)" id="create_room">Create room</a></b>
     </div>
 </div>`);
+
+    all_rooms.find("#create_room").click(function () {
+
+        console.log("new room");
+    });
+
+    const opened_rooms = {};
 
     for (const key in rooms) {
         const room = rooms[key];
         all_rooms.find("#" + room.m_uuid).click(function () {
-            console.log("X", room);
+
+            if (opened_rooms[room.m_uuid] === undefined) {
+                opened_rooms[room.m_uuid] = {
+                    top: 200,
+                    left: 200,
+                    content: ""
+                }
+            }
+
+            const R = opened_rooms[room.m_uuid];
+            if (R.visible) {
+                R.elem.remove();
+                delete R.elem;
+                R.visible = false;
+                return;
+            }
+            R.visible = true;
 
             const the_room = $(`
 <div class="meta_room my_draggable">
-    <div>
         <div class="my_resizable">
-            <h1>Room: witoza</h1>
+        
+            <div style="background-color: #f1a899; padding: 2px">
+                <strong>` + key + " (owner " + room.owner.username + `)</strong>
+                <strong style="float:right"><a href="javascript:void(0)" id="close">x</a>&nbsp;</strong>
+            </div>
+            <div class="my_clearfix"></div>
+            Chat:
+            <div style="border: 1px solid black; width: 100%; height: 200px; overflow:auto; background-color: white;" id="mcontent"></div>
+            Say what:
+            <input type="text" style="width: 100%; " id="saywhat"></input>
             
-            <form action="demo_form.asp">
-              First name: <input type="text" name="fname"><br>
-              Last name: <input type="text" name="lname"><br>
-              <input type="submit" value="Submit">
-            </form>
+            <input type="submit" value="Send" id="send">
         </div>
-    </div>
 </div>`);
+
+            the_room.click(function () {
+                console.log(room);
+            });
+
+            the_room.find("#close").on('click', function () {
+                R.elem.remove();
+                delete R.elem;
+                R.visible = false;
+            });
+
+            the_room.find("#send").on('click', function () {
+                console.log("send");
+                const what = the_room.find("#saywhat").val();
+                if (what == "") {
+                    return;
+                }
+
+                R.content = "<p style='margin:2px;'><b>" + user.username + "</b>: " + what + "</p>" + R.content;
+
+                the_room.find("#saywhat").val("");
+                update_content();
+            });
+
+            function update_content() {
+                the_room.find("#mcontent").html(R.content);
+            }
+
+            update_content();
+
+            the_room.css({top: R.top, left: R.left, position: 'fixed'});
 
             $("body").prepend(the_room);
 
-            $('.my_draggable').draggable();
+            the_room.draggable({
+                stop: function (event, ui) {
+                    R.top = $(this).offset().top;
+                    R.left = $(this).offset().left;
+                }
+            });
             $('.my_resizable').resizable();
+
+            R.elem = the_room;
 
         });
     }
@@ -95,8 +163,7 @@ $(document).ready(function () {
     });
 
     $("body").prepend(all_rooms);
-    $('.my_draggable').draggable();
-    $('.my_resizable').resizable();
+    all_rooms.draggable();
 
 });
 
